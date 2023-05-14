767af6c53cf998e69ae128a56b548328790da89e
codeMasterFM <Franklynp87@gmail.com>
2023-05-14 13:14:39 +0800

    First comit

---
+++ wwwroot/js/DashBoard.js
@@ -0,0 +1,8 @@
+
+const nn = document.querySelector("#NN");
+const actualizarVista = () => {
+    window.location.href = "/homev";
+    console.log("hola");
+}
+
+nn.addEventListener("click", actualizarVista)
